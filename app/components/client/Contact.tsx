8e27ffc413b66ea5de02d4d99861d585bc01b84b
JohnPaulWeb <braganzajohnpaul@gmail.com>
2025-02-13 11:14:56 +0800

    Adding | Contact

--- app/components/client/Contact.tsx
+++ app/components/client/Contact.tsx
@@ -1,7 +1,45 @@
+"use client"
 
+import { useRef, useState } from "react"
+import emailjs from '@emailjs/browser'
+import { EMAILJS_PUBLIC_KEY, EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID } from "@/lib/emailjs"
 
 
 export default function Contact() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        message: ''
+    })
+
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [showPopup, setShowPopup] = useState(false)
+    const [isSuccess, setIsSuccess] = useState(false)
+    const [errorMessage, setErrorMessage] = useState('')
+    const formRef = useRef<HTMLFormElement>(null)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target
+        setFormData(prev => ({ ...prev, [name]: value }))
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault
+        setIsSubmitting(true)
+        setErrorMessage('')
+
+        try {
+            const result = await emailjs.sendForm(
+                EMAILJS_SERVICE_ID,
+                EMAILJS_TEMPLATE_ID,
+                formRef.current!,
+                EMAILJS_PUBLIC_KEY
+            )
+
+            console.log('EmailJS result:', result.text)
+        }
+    }
     return (
         <section>
             
--- lib/emailjs.ts
+++ lib/emailjs.ts
@@ -1,3 +1,13 @@
+
 import { init } from '@emailjs/browser';
 
+
 export const EMAILJS_SERVICE_ID = "service"
+export const EMAILJS_TEMPLATE_ID = "template"
+
+
+export const EMAILJS_PUBLIC_KEY = ""
+
+export const initEmailJS = () => {
+    init(EMAILJS_PUBLIC_KEY);
+}
